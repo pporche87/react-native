4366e690c3c2185b7b4ce6a947cea8fae9b6c721
Patrick <porchewellness@gmail.com>
2017-05-16 13:14:25 -0700

    finishes package.json adds albums

---
+++ albums/index.ios.js
@@ -0,0 +1,14 @@
+// Indx.ios.js - place code in here for IOS!!!
+
+// Import a library to help create a component
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import Header from './src/components/header'
+
+// Create a component
+const App = () => (
+	<Header headerText={'Albums!'}/>
+)
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App)
