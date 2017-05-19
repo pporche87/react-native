5217d5c4d1be856f6630d6f1fdaa85676bd5bb38
Patrick <porchewellness@gmail.com>
2017-05-19 10:39:07 -0700

    finishes specs

--- gymFinder/__tests__/GymDetail_test.js
+++ gymFinder/__tests__/GymDetail_test.js
@@ -1,12 +1,20 @@
 import 'react-native'
 import React from 'react'
 import GymDetail from '../app/GymDetail'
+import axios from 'axios'
 
 import renderer from 'react-test-renderer'
 
+const customHeader = {
+	headers: { 'Authorization': 'bearer m-WMwsZfIQPmgPFiBHOGFAlLZ5Rb6zNu0BfXPDEenRX-WX4EW1aXpE9ZiBIU4f1wcueIbfqJbNNIfbJ366hKVIxSg6CawC914ePZPUHTBxQDeAcrBNMqLUjMFDEaWXYx' }
+}
+
 it.only('renders correctly', () => {
-  const detail = renderer.create(
-    <GymDetail />
-  ).toJSON()
-	expect(detail).toMatchSnapshot()
-})
+	axios.get('https://api.yelp.com/v3/businesses/search?term=gyms&location=San+Francisco', customHeader)
+		.then(response => {
+			const detail = renderer.create(
+			 <GymDetail key={response.data.businesses.id[0]} business={response.data.businesses[0]} />
+		 ).toJSON()
+		 expect(detail).toMatchSnapshot()
+	 })
+	})
